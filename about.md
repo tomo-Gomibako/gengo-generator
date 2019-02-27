9f41983adc18df87bf4269a5b3d32494908b5a79
tomon9086 <tomo.mouse.9086@gmail.com>
2019-02-27 18:08:36 +0900

    initial commit

---
+++ about.md
@@ -0,0 +1,7 @@
+1. In japan, new era name will be announced on April 1st. So I am building app predicts it. Twitter's API will be used to post predictions.
+
+2. No. It just tweets generated sentences.
+
+3. Yes. It involves Tweeting, but it does not interfere with other users
+
+4. Twitter data will not be displayed. It just tweets.
---
+++ index.js
@@ -0,0 +1,18 @@
+const twitter = require("./twitter")
+const dailyuse = require("./dailyuse-kanji.json").dailyuse
+const kanji = dailyuse
+
+async function post() {
+	// console.log(await twitter.getTimelineByScreenName("71aa55"))
+	// console.log(kanji)
+	let status = ""
+	for(let i = 0; i < 2; i++) {
+		const index = Math.random() * kanji.length | 0
+		status += kanji[index]
+	}
+	// console.log(status)
+	twitter.postStatus(status)
+}
+setInterval(() => {
+	post()
+}, 900000);
